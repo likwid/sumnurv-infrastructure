d9ff313e6debacba52f7f59ad22c36ce641a4c6e
Jeff Gonzalez <likwid@gmail.com>
2015-05-16 12:51:20 -0500

    Added vpc and internal dns

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,70 +2,34 @@ provider "aws" {
   region = "${var.region}"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "172.22.0.0/16"
+resource "aws_vpc" "ci" {
+  cidr_block = "172.16.0.0/16"
   enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags {
-    Name = "Example VPC"
+    Name = "NVentio CI"
+    Environment = "ci"
   }
 }
 
-# Create a private subnet in two AZs
-resource "aws_subnet" "private-subnet-1" {
-  availability_zone = "${var.region}b"
-  vpc_id = "${aws_vpc.example.id}"
-  cidr_block = "172.22.100.0/24"
-  tags {
-    Name = "Private #1"
-  }
-}
-
-resource "aws_subnet" "private-subnet-2" {
-  availability_zone = "${var.region}c"
-  vpc_id = "${aws_vpc.example.id}"
-  cidr_block = "172.22.101.0/24"
-  tags {
-    Name = "Private #2"
-  }
-}
-
-# Create a public subnet in two AZs
 resource "aws_subnet" "public-subnet-1" {
   availability_zone = "${var.region}b"
-  vpc_id = "${aws_vpc.example.id}"
-  cidr_block = "172.22.10.0/24"
+  vpc_id = "${aws_vpc.ci.id}"
+  cidr_block = "172.16.10.0/24"
   map_public_ip_on_launch = true
   tags {
     Name = "Public #1"
   }
 }
 
-resource "aws_subnet" "public-subnet-2" {
-  availability_zone = "${var.region}c"
-  vpc_id = "${aws_vpc.example.id}"
-  cidr_block = "172.22.11.0/24"
-  map_public_ip_on_launch = true
-  tags {
-    Name = "Public #2"
-  }
-}
-
-# Create an Internet Gateway
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.example.id}"
+  vpc_id = "${aws_vpc.ci.id}"
 }
 
-# Create NAT instance
-resource "aws_instance" "nat" {
-  ami = "${lookup(var.nat_amis, var.region)}"
-  instance_type = "t1.micro"
-  subnet_id = "${aws_subnet.public-subnet-1.id}"
-  key_name = "likwid"
-  associate_public_ip_address = true
-  source_dest_check = false
-  tags {
-    Name = "NAT #1"
-  }
+/*
+resource "aws_route53_zone" "ci" {
+  name = "ci.nventio.int"
+  vpc_id = "${aws_vpc.ci.id}"
 }
+*/
